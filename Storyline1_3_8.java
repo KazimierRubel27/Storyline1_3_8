4caa4fbe10ead3bd8893348ace9e16bc9f04fa88
Kaz <kazimier.rubel27@student.tamdistrict.org>
2024-10-21 15:02:35 -0700

    final commit

--- Storyline1_3_8.java
+++ Storyline1_3_8.java
@@ -14,39 +14,59 @@ public class Storyline1_3_8 {
             System.out.println("B: " + optionTwo);
             choice = userChoice.nextLine().toUpperCase();
         }
-
         return choice;
     }
 
     public static void main(String[] args) {
-        System.out.println("You are a special agent working for the CIA. " +
-                "Your mission is to diffuse a bomb that has been placed on the top of the Empire State Building. " +
-                "After scaling the side, you reach the bomb. What do you do next?");
+        Scanner playChoice = new Scanner(System.in);
+        String play = "Y";
+        while (play.equals("Y")) {
+            System.out.println("You are a special agent working for the CIA. " +
+                    "Your mission is to diffuse a bomb that has been placed on the top of the Empire State Building. " +
+                    "After scaling the side, you reach the bomb. What do you do next?");
 
-        String choice = twoChoices("Call the expert before touching the bomb",
-                "Waste no time and take out your defusing tools");
+            String choice = twoChoices("Call the expert before touching the bomb",
+                    "Waste no time and take out your defusing tools");
 
-        if (choice.equals("A")) {
-            System.out.println("You call the expert, but they don't pick up, and you waste precious time.");
-            System.out.println("You now have to cut a wire without help.");
-            choice = twoChoices("Cut red wire", "Cut yellow wire");
             if (choice.equals("A")) {
-                System.out.println(
-                        "You hear a loud noise and think you cut the wrong wire, but it was just a bird, nothing happens");
+                System.out.println("You call the expert, but they don't pick up, and you waste precious time.");
+                System.out.println("You now have to cut a wire without help.");
+                choice = twoChoices("Cut red wire", "Cut yellow wire");
+                if (choice.equals("A")) {
+                    System.out.println(
+                            "You hear a loud noise and think you cut the wrong wire, but it was just a bird, nothing happens");
+                } else {
+                    System.out.println("Nothing happens");
+                }
+                System.out.println("Now you have one wire left");
+                choice = twoChoices("Wait for the time to expire", "Cut the other wire");
+                if (choice.equals("A")) {
+                    System.out.println(
+                            "You let time expire and in the last 5 seconds you here fast beeping and the bomb explodes");
+                } else {
+                    System.out.println(
+                            "You cut the other wire, and a deactivation sound plays, the bomb was succesfully disabled");
+                }
             } else {
-                System.out.println("Nothing happens");
-            }
-            System.out.println("Now you have one wire left");
-            choice = twoChoices("Wait for the time to expire", "Cut the other wire");
-            if (choice.equals("A")) {
                 System.out.println(
-                        "You let time expire and in the last 5 seconds you here fast beeping and the bomb explodes");
-            } else {
-                System.out.println(
-                        "You cut the other wire, and a deactivation sound plays, the bomb was succesfully disabled");
+                        "You trip and your tools snag a yellow wire, cutting it. Luckily, the bomb doesn't blow up.");
+                System.out.println("You now are left with a bomb with one wire cut");
+                choice = twoChoices("You cut the the remaining red wire", "You wait for time to expire");
+                if (choice.equals("A")) {
+                    System.out.println(
+                            "You cut the red wire, and the bomb immideatly explodes, killing everyone including you");
+                } else {
+                    System.out.println(
+                            "You cut the other wire, and a deactivation sound plays, the bomb was succesfully disabled");
+                }
             }
-        } else {
-            System.out.println("You trip and your tools snag a wire, cutting it. Luckily, the bomb doesn't blow up.");
+            System.out.print("\nWould you like to play again?");
+            String playTwo = playChoice.nextLine().toUpperCase();
+            if (play.length() > 0) {
+                play = playTwo.substring(0, 1);
+            }
         }
+        playChoice.close();
     }
+
 }
